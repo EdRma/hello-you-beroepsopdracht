bbd3f026640c5a8f8efef12cb2341af8916b7c0a
EdRma <34298@ma-web.nl>
2021-10-19 14:26:35 +0200

    derde draft

--- Hello you beroepsopdracht.py
+++ Hello you beroepsopdracht.py
@@ -1,5 +1,7 @@
 print("stel je voor...")
-print("je bent thuis omdat je ziek was terwijl je moder je broertje en zusje naar school brengt.")
+print("het is 1989 en je bent 7 jaar oud")
+print("de Sovjet-Unie is net uit jullie land vertrokken en er is al een tijd een burgeroorlog gaanden")
+print("je zit ziek thuis terwijl je moeder je broertje en zusje naar school brengt.")
 print("je zit op de vloer te spelen met je speelgoed...")
 print("opeens komt je vader binnen en zegt dat je je spullen moet inpakken en dat jullie moeten vertrekken.")
 print("je luistert naar je vader en pakt je favoriete knuffel in samen met essentiële spullen.")
@@ -8,22 +10,34 @@ print("hij loopt met je naar de deur terwijl jij schreewt dat ze moeten wachten 
 antwoord1 = input ("je vader zegt dat er geen tijd is om te wachten en dat je moet meegaan. wat kies jij?  ")
 
 if antwoord1.lower()== "meegaan":
-    print("je gaat naar beneden om een broodje te maken en op je ipad te kijken of er nog iets is gebeurd")
-    antwoord2 = input("ga je nu douchen of je spullen inpakken? typ D of S: ")
+    print("je loopt met je vader je dorp uit, weg van huis.")
+    print("je bent nog steeds in de war waarom jullie moesten vertrekken van jullie huis. ")
+    print("jullie komen aan bij een controlepost.")
+    print("er wordt jullie gevraagd aan wie onze leider is")
+    print("je vader zegt dat Maulana Samiul Haq zijn vader is, terwijl je die naam helemaal niet kent.")
+    
 
-    if antwoord2.lower()=="D":
-        print("je stapt onder de douche en kleed je daarna aan")
-        print("je loopt naar het station maar bent erachter gekomen dat je hem hebt gemist hierdoor zal je telaat komen en is nu het spel voorbij")
+    antwoord2 = input("zal je de waarheid vertellen aan de vreemdelingen, of vertrouw je je vader en lieg je met hem mee?")
 
-    elif antwoord2.lower()=="S":
-        print("je pakt je spullen in en kleed je aan omdat het al bijna 8 uur is")
-        print("je loopt naar de trein en stapt in onderweg naar amsterdam sloterdijk")
-        print("je komt aan op amsterdam slooterdijk en loopt naar het mediacollege")
-        print("je hebt het optijd gehaald en dus het spel gewonnen")
+    if antwoord2.lower()=="liegen":
+        print("")
+        print("")
+        print("")
+        print("")
 
+        antwoord3 = input("")
+    
+    elif antwoord2.lower()=="waarheid":
+        print("")
+        print("")
+        print("")
+        print("")
 
+        antwoord3 = input("")
 
-elif antwoord1.lower() == "wachten":
+elif antwoord1.lower() == "wachten": #einde 1 van de 4
+    print("")
+    print("")
     print("je zegt dat je gaat wachten op je familie...")
     print("je vader kijkt je bedroefd aan en geeft je een kus op je voorhoofd.")
     print("hij loopt de deur uit en sluit hem achter zich.")
